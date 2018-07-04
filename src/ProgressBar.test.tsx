9e1ebf8d9f73583f31505c8f183251de3a478f3f
Manikya Vinay <vrali@live.com>
2018-07-04 17:14:11 -0400

    tests

--- src/ProgressBar.test.tsx
+++ src/ProgressBar.test.tsx
@@ -1,12 +1,43 @@
+import * as Enzyme from "enzyme";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { ProgressBar, ProgressBarMode } from "./ProgressBar";
+import { ProgressBar } from "./ProgressBar";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(
-    <ProgressBar mode={ProgressBarMode.percentage} value={10} />,
-    div
-  );
+  ReactDOM.render(<ProgressBar value={10} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("default mode is percentage", () => {
+  const progressBar = Enzyme.shallow(<ProgressBar value={10} />);
+  expect(
+    progressBar
+      .find(".progress-bar-progress-label")
+      .text()
+      .includes("%")
+  ).toBe(true);
+});
+
+it("shows error when value out of range", () => {
+  const progressBar = Enzyme.shallow(<ProgressBar minvalue={20} value={10} />);
+  expect(progressBar.find(".progress-bar-error").length).toBeTruthy();
+  expect(progressBar.find(".progress-bar").length).toBe(0);
+});
+
+it("maps 25 value to width 50% when range is 0 to 50", () => {
+  const progressBar = Enzyme.shallow(<ProgressBar maxvalue={50} value={25} />);
+  expect(
+    progressBar
+      .find(".progress-bar")
+      .html()
+      .includes('style="width:50%"')
+  ).toBeTruthy();
+});
+
+it("hide value label when showvaluelabel is false", () => {
+  const progressBar = Enzyme.shallow(
+    <ProgressBar showvaluelabel={false} maxvalue={50} value={25} />
+  );
+  expect(progressBar.find(".progress-bar-progress-label").text()).toBeFalsy();
 });
